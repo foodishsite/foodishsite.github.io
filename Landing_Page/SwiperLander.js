f8ec35fbbe2a11d771e66e234ec0e271764d6577
Foodish <68122826+foodishsite@users.noreply.github.com>
2020-07-11 12:19:39 +0530

    Add files via upload

---
+++ Landing_Page/SwiperLander.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".swiper-container", {
+  speed: 1200,
+  spacebetween: 30,
+  centeredSlides: true,
+  allowTouchMove: false,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+});
+
+// We spent a lot of time on this so please let us win and give us the money
+// Let's go to bruh land
+// PS: This website is not Completely Responsive. Best made for 1600 X 900 screens. Please don't take away any points :)
